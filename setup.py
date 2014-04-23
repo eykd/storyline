229ff80b7d38f12934723cc43bbe084c243054a2
deyk <deyk@crossway.org>
2014-04-22 22:22:54 -0700

    Split base requirements into setup.py, leaving dev-requirements in requirements.txt

--- setup.py
+++ setup.py
@@ -10,7 +10,20 @@ with open('./requirements.txt') as fi:
 setup(
     name = "storyline",
     version = "0.1",
-    install_requires = REQUIREMENTS,
+    install_requires = [
+        "Flask==0.10.1",
+        "Jinja2==2.7.2",
+        "Markdown==2.4",
+        "MarkupSafe==0.21",
+        "configobj==5.0.4",
+        "docopt==0.6.1",
+        "msgpack-python==0.4.2",
+        "path.py==5.1",
+        "smartypants==1.8.3",
+        "typogrify==2.0.4",
+        "watchdog==0.7.1",
+        "webassets==0.9",
+    ],
     packages = find_packages(),
     entry_points = {
         'console_scripts': [
